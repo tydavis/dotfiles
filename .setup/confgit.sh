01b97e0b07bc36d4410975f2b0d660e15fb96e20
Tyler Davis <tydavis@gmail.com>
2020-08-31 21:02:49 -0700

    git: add gh, switch back to nvim

--- .setup/confgit.sh
+++ .setup/confgit.sh
@@ -34,8 +34,10 @@ case `uname` in
   Darwin)
     # commands for OS X go here
     git config --global credential.helper osxkeychain
-    git config --global core.editor "code --wait"
-    git config --global difftool.cmd "code --wait --diff $LOCAL $REMOTE"
+    #git config --global core.editor "code --wait"
+    #git config --global difftool.cmd "code --wait --diff $LOCAL $REMOTE"
+    git config --global core.editor nvim
+    git config --global difftool.cmd "nvim -d $LOCAL $REMOTE $MERGED -c 'wincmd w' -c 'wincmd J'"
     git config --global core.excludesfile "/Users/tydavis/.gitignore_global"
     git config --global http.cookiefile /Users/tydavis/.gitcookies
   ;;
