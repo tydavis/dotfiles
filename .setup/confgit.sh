8e7364da4cac80c66a4c586027cf16c850dea385
Tyler Davis <tydavis@gmail.com>
2021-05-10 16:37:16 +0000

    vim: switch back to ye olde vim

--- .setup/confgit.sh
+++ .setup/confgit.sh
@@ -40,8 +40,8 @@ case `uname` in
     git config --global credential.helper osxkeychain
     # git config --global core.editor "code --wait"
     # git config --global difftool.cmd "code --wait --diff $LOCAL $REMOTE"
-    git config --global core.editor nvim
-    git config --global difftool.cmd "nvim -d $LOCAL $REMOTE $MERGED -c 'wincmd w' -c 'wincmd J'"
+    git config --global core.editor vim
+    # git config --global difftool.cmd "nvim -d $LOCAL $REMOTE $MERGED -c 'wincmd w' -c 'wincmd J'"
     git config --global core.excludesfile "/Users/tydavis/.gitignore_global"
     git config --global http.cookiefile /Users/tydavis/.gitcookies
   ;;
@@ -50,8 +50,8 @@ case `uname` in
     git config --global credential.helper "store --file=/home/tydavis/.config/gitcreds"
     # git config --global core.editor "code --wait"
     # git config --global difftool.cmd "code --wait --diff $LOCAL $REMOTE"
-    git config --global core.editor nvim
-    git config --global difftool.cmd "nvim -d $LOCAL $REMOTE $MERGED -c 'wincmd w' -c 'wincmd J'"
+    git config --global core.editor vim
+    # git config --global difftool.cmd "nvim -d $LOCAL $REMOTE $MERGED -c 'wincmd w' -c 'wincmd J'"
     git config --global core.excludesfile "/home/tydavis/.gitignore_global"
     git config --global http.cookiefile /home/tydavis/.gitcookies
   ;;
