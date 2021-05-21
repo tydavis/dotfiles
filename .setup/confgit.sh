16c76555f33637386bf03f2ace10fc91305277e8
Tyler Davis <tydavis@gmail.com>
2021-05-21 16:18:42 +0000

    git: vscode, remove newrelic repo

--- .setup/confgit.sh
+++ .setup/confgit.sh
@@ -24,34 +24,40 @@ git config --global status.submodulesummary 1
 git config --global pull.ff only
 git config --global hub.protocol https
 
-if [ "$(command -v delta)" ]; then
-  git config --global core.pager delta
-  git config --global interactive.diffFilter "delta --color-only"
-  git config --global delta.line-numbers true
-  # git config --global delta.side-by-side true
-  git config --global delta.side-by-side false
-  # git config --global delta.syntax-theme "Solarized (light)"
-  git config --global delta.syntax-theme "gruvbox-dark"
-fi
+# if [ "$(command -v delta)" ]; then
+#   git config --global core.pager delta
+#   git config --global interactive.diffFilter "delta --color-only"
+#   git config --global delta.line-numbers true
+#   # git config --global delta.side-by-side true
+#   git config --global delta.side-by-side false
+#   # git config --global delta.syntax-theme "Solarized (light)"
+#   git config --global delta.syntax-theme "gruvbox-dark"
+# fi
 
 case `uname` in
   Darwin)
     # commands for OS X go here
     git config --global credential.helper osxkeychain
-     git config --global core.editor "code --wait"
-     git config --global difftool.cmd "code --wait --diff $LOCAL $REMOTE"
     #git config --global core.editor vim
-    #git config --global difftool.cmd "nvim -d $LOCAL $REMOTE $MERGED -c 'wincmd w' -c 'wincmd J'"
+    git config --global core.editor "code --wait"
+    git config --global diff.tool vscode
+    git config --global difftool.vscode.cmd "code --wait --diff \$LOCAL \$REMOTE"
+    git config --global difftool.nvim.cmd "nvim -d \$LOCAL \$REMOTE \$MERGED -c 'wincmd w' -c 'wincmd J'"
+    git config --global merge.tool vscode
+    git config --global mergetool.vscode.cmd "code --wait \$MERGED"
     git config --global core.excludesfile "/Users/tydavis/.gitignore_global"
     git config --global http.cookiefile /Users/tydavis/.gitcookies
   ;;
   Linux)
     # commands for Linux go here
     git config --global credential.helper "store --file=/home/tydavis/.config/gitcreds"
-    git config --global core.editor "code --wait"
-    git config --global difftool.cmd "code --wait --diff $LOCAL $REMOTE"
     #git config --global core.editor vim
-    # git config --global difftool.cmd "nvim -d $LOCAL $REMOTE $MERGED -c 'wincmd w' -c 'wincmd J'"
+    git config --global core.editor "code --wait"
+    git config --global diff.tool vscode
+    git config --global difftool.vscode.cmd "code --wait --diff \$LOCAL \$REMOTE"
+    git config --global difftool.nvim.cmd "nvim -d \$LOCAL \$REMOTE \$MERGED -c 'wincmd w' -c 'wincmd J'"
+    git config --global merge.tool vscode
+    git config --global mergetool.cmd "code --wait \$MERGED"
     git config --global core.excludesfile "/home/tydavis/.gitignore_global"
     git config --global http.cookiefile /home/tydavis/.gitcookies
   ;;
